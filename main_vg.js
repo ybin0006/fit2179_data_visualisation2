7f93b87b9a66b0ca1a3c3326d33043cc318fb1f6
ybin0006 <ybin0006@student.monash.edu>
2026-05-21 01:53:54 +1000

    Initial upload of data visualisation projects

---
+++ main_vg.js
@@ -0,0 +1,36 @@
+
+
+// Line chart - Objects launches over time 
+var vg_1 = "charts/satellites_over_time.vg.json";
+
+vegaEmbed("#chart1", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+// Map - Launch sites map 
+var vg_2 = "charts/launch_sites_map.vg.json";
+
+vegaEmbed("#chart2", vg_2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+// treechart - No of satellites vs their purpose
+var vg_3 = "charts/satellites_purpose.vg.json";
+
+vegaEmbed("#chart3", vg_3).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+// treechart - No of satellites vs their purpose
+var vg_4 = "charts/country_purpose.vg.json";
+
+vegaEmbed("#chart4", vg_4).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+// treechart - No of satellites vs their purpose
+var vg_5 = "charts/owner_contractor.vg.json";
+
+vegaEmbed("#chart5", vg_5).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);

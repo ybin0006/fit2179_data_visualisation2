42bae3cf20ba05ab78158742c393a43de3adb469
ybin0006 <ybin0006@student.monash.edu>
2026-05-29 22:55:40 +1000

    create 3 more charts

--- main_vg.js
+++ main_vg.js
@@ -3,46 +3,90 @@
 // Line chart - Objects launches over time 
 var vg_1 = "charts/satellites_over_time.vg.json";
 
-vegaEmbed("#chart1", vg_1).then(function(result) {
+vegaEmbed("#chart1", vg_1, {
+    actions: false
+}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 // Map - Launch sites map 
 var vg_2 = "charts/launch_sites_map.vg.json";
 
-vegaEmbed("#chart2", vg_2).then(function(result) {
+vegaEmbed("#chart2", vg_2, {
+    actions: false
+}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 // treechart - No of satellites vs their purpose
 var vg_3 = "charts/satellites_purpose.vg.json";
 
-vegaEmbed("#chart3", vg_3).then(function(result) {
+vegaEmbed("#chart3", vg_3, {
+    actions: false
+}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 // treechart - No of satellites vs their purpose
 var vg_4 = "charts/country_purpose.vg.json";
 
-vegaEmbed("#chart4", vg_4).then(function(result) {
+vegaEmbed("#chart4", vg_4, {
+    actions: false
+}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 // treechart - No of satellites vs their purpose
 var vg_5 = "charts/owner_contractor.vg.json";
 
-vegaEmbed("#chart5", vg_5).then(function(result) {
+vegaEmbed("#chart5", vg_5, {
+    actions: false
+}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_6 = "charts/aus_satellites_over_time.vg.json";
 
-vegaEmbed("#chart6", vg_6).then(function(result) {
+vegaEmbed("#chart6", vg_6, {
+    actions: false
+}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_7 = "charts/aus_mass_year.vg.json";
 
 vegaEmbed("#chart7", vg_7).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_8 = "charts/aus_commercial_purpose.vg.json";
+
+vegaEmbed("#chart8", vg_8, {
+    actions: false
+}).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_9 = "charts/aus_satelites_type.vg.json";
+
+vegaEmbed("#chart9", vg_9, {
+    actions: false
+}).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_10 = "charts/aus_satellites_operator.vg.json";
+
+vegaEmbed("#chart10", vg_10, {
+    actions: false
+}).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_11 = "charts/aus_facilities.vg.json";
+
+vegaEmbed("#chart11", vg_11, {
+    actions: false
+}).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
